0dabbf1cc492977f1110c10535c7061b5678a2c2
Alejandro Seguí <alesegdia@gmail.com>
2016-11-15 04:51:09 +0100

    updating backup game list

--- savegames.sh
+++ savegames.sh
@@ -22,3 +22,6 @@ USER=<YOUR-USER-HERE>
 backup_game "Borderlands2" "/home/$USER/.local/share/aspyr-media/borderlands 2/willowgame/savedata"
 backup_game "SystemShock2" "/home/$USER/.systemshock2/systemshock2/drive_c/Program Files/SystemShock2/save_"*
 backup_game "Minecraft" "/home/$USER/.minecraft/saves"
+backup_game "DeadIsland" "/home/$USER/.local/share/Steam/userdata/73433123/91310/remote"
+backup_game "Messiah" "/home/$USER/.wine/drive_c/GOG Games/Messiah/SAVE"
+backup_game "BaldursGate_EnhancedEdition" "home/$USER/.local/share/Baldur's Gate - Enhanced Edition"
